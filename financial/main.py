3e7af63aa0a160756709237d6155f3d0bd0e8a7f
WanHsuan <audrey.35921@gmail.com>
2023-07-30 23:07:14 +0800

    Update title and description

--- financial/main.py
+++ financial/main.py
@@ -1,11 +1,10 @@
-# from dotenv import load_dotenv
-
 from fastapi import FastAPI
 
 from financial.routers import router
 
 
-# load_dotenv()
-
-app = FastAPI()
+app = FastAPI(
+    title="Python Assignment",
+    description="""This project is designed to retrieve and manage financial data for specific stocks (IBM, Apple Inc.) using the [AlphaVantage](https://www.alphavantage.co/documentation/) API. It provides functionality to fetch the recent two weeks of financial data, process and store it in a local database, and offers API endpoints to retrieve records and perform statistical calculations on the stored data. The APIs are implemented using `FastAPI`, enabling a fast and robust interface.""",
+)
 app.include_router(router)
