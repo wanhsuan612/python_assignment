a2f79e0773557159e164aa3125234fd7d574dfd9
WanHsuan <audrey.35921@gmail.com>
2023-07-30 22:23:52 +0800

    Update database connect module

--- financial/config/database.py
+++ financial/config/database.py
@@ -9,7 +9,7 @@ host = os.getenv("DB_HOST")
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 database = os.getenv("DB_DATABASE")
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}/{database}"
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
