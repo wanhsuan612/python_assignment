2b416539a54da7d5431f353781d7aa8e4cee959e
WanHsuan <audrey.35921@gmail.com>
2023-07-30 23:25:47 +0800

    Update docstring

--- financial/services/financial_data.py
+++ financial/services/financial_data.py
@@ -9,6 +9,19 @@ from financial.utils.exceptions import DateNotFound, SymbolNotFound
 
 @lru_cache()
 def get_financial_data(start_date=None, end_date=None, symbol=None):
+    """Retrieve financial data based on the provided filtering criteria.
+
+    This function queries the FinancialData table and applies optional filters
+    for symbol, start_date, and end_date.
+
+    Args:
+        start_date (date, optional): The starting date of the data retrieval (inclusive).
+        end_date (date, optional): The ending date of the data retrieval (inclusive).
+        symbol (str, optional): The stock symbol to filter by.
+
+    Returns:
+        list[FinancialData]: A list of FinancialData objects that match the filtering criteria.
+    """
     session = SessionLocal()
     _query = session.query(FinancialData)
 
@@ -24,6 +37,18 @@ def get_financial_data(start_date=None, end_date=None, symbol=None):
 
 
 def _filter_symbol(query: Query, value):
+    """Filter the query based on a stock symbol.
+
+    Args:
+        query (Query): The initial query object.
+        value (str): The stock symbol to filter by.
+
+    Returns:
+        Query: A modified query object filtered by the stock symbol.
+
+    Raises:
+        SymbolNotFound: If no data is found for the given symbol.
+    """
     _query = query.filter(FinancialData.symbol == value)
     _d = _query.all()
     if len(_d) == 0:
@@ -32,6 +57,18 @@ def _filter_symbol(query: Query, value):
 
 
 def _filter_start_date(query: Query, value):
+    """Filter the query based on the start date.
+
+    Args:
+        query (Query): The initial query object.
+        value (date): The starting date to filter by.
+
+    Returns:
+        Query: A modified query object filtered by the start date.
+
+    Raises:
+        DateNotFound: If no data is found for the given start date.
+    """
     _query = query.filter(FinancialData.date >= value)
     _d = _query.all()
     if len(_d) == 0:
@@ -41,6 +78,18 @@ def _filter_start_date(query: Query, value):
 
 
 def _filter_end_date(query: Query, value):
+    """Filter the query based on the end date.
+
+    Args:
+        query (Query): The initial query object.
+        value (date): The ending date to filter by.
+
+    Returns:
+        Query: A modified query object filtered by the end date.
+
+    Raises:
+        DateNotFound: If no data is found for the given end date.
+    """
     _query = query.filter(FinancialData.date <= value)
     _d = _query.all()
     if len(_d) == 0:
@@ -50,6 +99,14 @@ def _filter_end_date(query: Query, value):
 
 
 def get_valid_dates(query: Query):
+    """Retrieve the minimum and maximum dates from the FinancialData table.
+
+    Args:
+        query (Query): The initial query object.
+
+    Returns:
+        Tuple[date, date]: A tuple containing the minimum and maximum dates present in the FinancialData table.
+    """
     row_with_min_date = query.order_by(FinancialData.date).first()
     row_with_max_date = query.order_by(FinancialData.date.desc()).first()
     return row_with_min_date.date, row_with_max_date.date
--- financial/services/statistics.py
+++ financial/services/statistics.py
@@ -2,16 +2,31 @@ import math
 
 from financial.models.financial_data import FinancialData
 
-
 def cal_average(data: list[FinancialData] = []):
-    average_op = 0
-    average_cp = 0
-    average_v = 0
-    if len(data) > 0:
-        op_list = [float(d.open_price) for d in data]
-        cp_list = [float(d.close_price) for d in data]
-        v_list = [int(d.volume) for d in data]
-        average_op = round(sum(op_list) / len(op_list), 2)
-        average_cp = round(sum(cp_list) / len(cp_list), 2)
-        average_v = math.ceil(sum(v_list) / len(v_list))
+    """
+    Calculate the average open price, close price, and volume from a list of financial data.
+
+    Args:
+        data (list[FinancialData], optional): A list of FinancialData objects containing the
+                                             open price, close price, and volume. Defaults to an empty list.
+
+    Returns:
+        tuple: A tuple containing three floats, representing the average open price (rounded to two decimal places),
+               the average close price (rounded to two decimal places), and the average volume (rounded up).
+
+    """
+    if not data:
+        return 0, 0, 0
+
+    total_op, total_cp, total_v = 0, 0, 0
+    for d in data:
+        total_op += float(d.open_price)
+        total_cp += float(d.close_price)
+        total_v += int(d.volume)
+
+    n = len(data)
+    average_op = round(total_op / n, 2)
+    average_cp = round(total_cp / n, 2)
+    average_v = math.ceil(total_v / n)
+
     return average_op, average_cp, average_v
